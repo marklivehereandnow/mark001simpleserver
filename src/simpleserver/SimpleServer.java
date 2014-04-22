cd644cdb361dcda024303d20532a9f7063ac1c37
Mark Chenpingling <mark@livehereandnow.com>
2014-04-22 08:40:48 +0800

    ver 0.2 ===  2014-4-22, 8:40, by Mark inform client about its IP

--- src/simpleserver/SimpleServer.java
+++ src/simpleserver/SimpleServer.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package simpleserver;
 
 import java.io.DataOutputStream;
@@ -11,24 +5,20 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- *
- * @author mark
- */
 public class SimpleServer {
+
     static ServerSocket serverSocket;
     static Socket socket;
     static DataOutputStream out;
-    
+
     public static void main(String[] args) throws IOException {
         System.out.println("starting server...");
-        serverSocket=new ServerSocket(7777);
+        serverSocket = new ServerSocket(7777);
         System.out.println("server started");
-        socket=serverSocket.accept();
-        System.out.println("connection from:"+ socket.getInetAddress());
-        out=new DataOutputStream(socket.getOutputStream());
-        out.writeUTF("This is a test of Java sockets, message from server.");
+        socket = serverSocket.accept();
+        System.out.println("connection from:" + socket.getInetAddress());
+        out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF("This is a test of Java sockets, message from server.Your IP is "+socket.getInetAddress());
         System.out.println("Data has been sent successfully");
     }
-    
 }
